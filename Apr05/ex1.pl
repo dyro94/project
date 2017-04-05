489436702e996ccbf8b70312798fd84017db7a23
dyro94 <roo4rohan@gmail.com>
2017-04-05 14:34:19 +0530

    array slice example

--- Apr05/ex1.pl
+++ Apr05/ex1.pl
@@ -1,4 +1,17 @@
-      use strict;
-      use warnings;
-      print "hi this is rohan!\n";
       
+     
+      my @text=qw(this is garden city bangalore);
+      print "before change:@text\n";
+      $text[3]='village';
+      print"replace city to village:@text\n";
+      $text[$text]='that';
+      print "change this to that:@text\n";
+      #in one statement copy last four element of the array
+      ($one,$two,$three,$four)=@text[-4..-1];
+      print "last four element of the array:$one $two $three $four\n";
+      ($ascalar,@array)=@text[1..3];
+      print "print first three element in scalar form:$ascalar\n";
+      print "print first three element in array form:@array\n";
+      @text=(@array,@text);
+      print "this is marged array:@text\n";
+
