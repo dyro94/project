2eab6a24848c1f4858f63b928325469c89df6a26
dyro94 <roo4rohan@gmail.com>
2017-04-07 12:43:06 +0530

    code on Apr06

---
+++ Apr06/mat_dig_sum.pl
@@ -0,0 +1,17 @@
+        use strict;
+        use warnings;
+          my $a=<>;
+         my  @row;
+         my @col;
+         
+       
+        for (my $i=0;$i<$a;$i++ ){
+            for(my $j=0; $j<$a; $j++){
+                   
+                    my  $input=<>;
+                             
+
+  
+        
+ 
+             
---
+++ Apr06/scalar_fun1.pl
@@ -0,0 +1,4 @@
+     use warnings;
+     use strict;
+     my @number = (1,2,3,4,5);
+     print "Numbers:",join('; ',@number),"
