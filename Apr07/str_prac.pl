7b282e11b920fb4cdbf69f66bfca5abefcfebf1d
dyro94 <roo4rohan@gmail.com>
2017-04-07 14:22:10 +0530

    add the question inside pod

--- Apr07/str_prac.pl
+++ Apr07/str_prac.pl
@@ -1,9 +1,31 @@
+=pod
+Find the word 'exceleron' from the input string
+Eg: 
+String 'ehxociielhlsehrkionu' does contain 'exceleron', but the string 'ehxociielhlshrkionu' does not(it's missing a third 'e' in 'exceleron')
+
+Condition: Let p0,p1,..p18 be the respective indices of 'e,h,x,o,c,i,i,e,l,h,l,s,h,r,k,i,o,n,u' in string S. 
+If p0 < p1 < p2 < .. <p9 is true, then String contains 'exceleron'.
+
+print 'YES' if the input string contains the word 'exceleron'; otherwise, print 'NO'.
+
+Sample input:
+eoxiecreloiirpomn 
+
+Output: 
+NO
+
+Explanation:
+The string 'eoxiecreloiirpomn' has the word 'exceleron' but it does not satisfy the condition. So the output will be 'NO'
+
+
+
 use warnings;
 use strict;
 
 
 
 print "enter the text-->\n";
+=cut
      
 chomp(my $var=<>);
        
