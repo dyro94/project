3e893fc31647445319d635a532161db1f5d08f8b
dyro94 <roo4rohan@gmail.com>
2017-05-08 17:09:13 +0530

    regular expression

--- May05/Up_Low_Case_rex.pl
+++ May05/Up_Low_Case_rex.pl
@@ -1,18 +1,20 @@
+
 =pod
  Regex Exercises:
 1. Write a regex to find out if the first character of a string is uppercase or not
     - Print "True" or "False" along with the character which is in uppercase
 =cut 
- use strict;
- use warnings;
 
+use strict;
+use warnings;
 
 print "ENTER THE STRING!!------>\n";
 
-my $user_input=<>;
-if ($user_input=~/(^[A-Z])/){
+my $user_input = <>;
+if ( $user_input =~ /(^[A-Z])/ ) {
     print "True-uppercase character is $1\n";
-}else {
-   print "False-first character is lowercase \n";
 }
-   
+else {
+    print "False-first character is lowercase \n";
+}
+
--- May05/credit_card_validation.pl
+++ May05/credit_card_validation.pl
@@ -1,81 +1,77 @@
+
 =potring_con;
 
 2. Credit Card validation ( XXXX-XXXX-XXXX-XXXX -> Read about credit cards and actual numbers they can contain )
     - Print "Valid Card" or "Invalid Card"
 =cut
+
 use strict;
 use warnings;
-  
+
 use Scalar::Util qw(reftype);
 print "Enter the Credit Card no:-\n";
-my $user_input=<>;
+my $user_input = <>;
 chomp($user_input);
-if($user_input=~/\d{4}-\d{4}-\d{4}-\d{4}/){
-      $user_input =~ s/-//g;    
-      my @split_each_char=split(//,$user_input);
-         #print " @split_each_char\n";
-           my $count=0;
-            my $string_con;
-           my $grand_total1=0;
-           my $grand_total2=0;
-           my $grand_total=0;
-           my $double;
-           my $total=0;
-            my $total1=0;
+if ( $user_input =~ /\d{4}-\d{4}-\d{4}-\d{4}/ ) {
+    $user_input =~ s/-//g;
+    my @split_each_char = split( //, $user_input );
 
-       foreach my $i (0..$#split_each_char){
-              $count++;
-           my $int_value=$split_each_char[$i]+1-1;
-            
-           
-            
-            if($count%2==0){
-                $double=$int_value+$int_value;
-                  
-                  
-                   
-		  
-                     if ($double=~/\d{2}/){
-                            $string_con="$double";
-                          my @digit=split(//,$string_con);
-                             
-                                 
-                                my $int;
-                                my $temp;
-                            foreach my $i(0..$#digit){
-                                 $temp = $digit[$i];
-                               $int=$temp+1-1;
-                                 
-                                $total=$total+$int;
-                            
+    #print " @split_each_char\n";
+    my $count = 0;
+    my $string_con;
+    my $grand_total1 = 0;
+    my $grand_total2 = 0;
+    my $grand_total  = 0;
+    my $double;
+    my $total  = 0;
+    my $total1 = 0;
+
+    foreach my $i ( 0 .. $#split_each_char ) {
+        $count++;
+        my $int_value = $split_each_char[$i] + 1 - 1;
+
+        if ( $count % 2 == 0 ) {
+            $double = $int_value + $int_value;
+
+            if ( $double =~ /\d{2}/ ) {
+                $string_con = "$double";
+                my @digit = split( //, $string_con );
+
+                my $int;
+                my $temp;
+                foreach my $i ( 0 .. $#digit ) {
+                    $temp = $digit[$i];
+                    $int  = $temp + 1 - 1;
+
+                    $total = $total + $int;
+
                 }
-                 
 
-              
- 
-           }else{
-            
-             
-           $total1=$total1+$double;
-    
+            }
+            else {
+
+                $total1 = $total1 + $double;
+
+            }
+            $grand_total1 = $total + $total1;
+
+        }
+        else {
+            $grand_total2 += $int_value;
+
+        }
+
+    }
+    $grand_total = $grand_total1 + $grand_total2;
+    print "$grand_total\n";
+    if ( $grand_total % 10 == 0 ) {
+        print "Valid Credit Card\n";
+    }
+    else {
+        print "Invalid Credit Card\n";
+    }
 }
-$grand_total1=$total+$total1;
-
-
-}else{
- $grand_total2+=$int_value;
-
+else {
+    print "Invalid Credit Card\n";
 }
-
-}
-$grand_total=$grand_total1+$grand_total2;
-print "$grand_total\n";
-if($grand_total%10==0){
-    print "Valid Credit Card\n";
-  }else{
-     print "Invalid Credit Card\n";
-}
-}else{
-   print "Invalid Credit Card\n";
- }
 
