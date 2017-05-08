c14fd1296fd1fd41f8f7dc7436ce9ad606ae7f3f
dyro94 <roo4rohan@gmail.com>
2017-05-08 18:44:17 +0530

    regular ecxpression

--- May05/White_space.pl
+++ May05/White_space.pl
@@ -1,8 +1,17 @@
+
+=pod
+3. Write a regex to:
+    - Remove white-space from start and end position
+    - Convert 2 or more spaces to 1
+    - Exclude newline with a start spacing
+    - Print the output after the above operations
+=cut
+
 use strict;
 use warnings;
 print "Enter the input----->\n";
 my $user_input = <>;
 chomp($user_input);
 $user_input =~ s/^\s+|\s+$//g;
-$user_input =~ s/ +/ /g;
+$user_input =~ s/\s+/ /g;
 print "$user_input\n";
--- May05/d_m_y.pl
+++ May05/d_m_y.pl
@@ -10,6 +10,7 @@ use warnings;
 
 print "Enter the input------>\n";
 chomp( my $user_input_1 = <> );
+my $output_date = $user_input_1;
 if ( $user_input_1 =~ /(\d{2}-\d{2}-\d{4}|\d{2}\/\d{2}\/\d{4}|\d{8})/ ) {
 
     #print "the user date is $1  \n";
@@ -22,19 +23,21 @@ if ( $user_input_1 =~ /(\d{2}-\d{2}-\d{4}|\d{2}\/\d{2}\/\d{4}|\d{8})/ ) {
     my ( $mon_1_digit, $mon_2_digit ) = splice( @array, 2, 2 );
 
     my $month = $mon_1_digit * 10 + $mon_2_digit;
-
-    if ( $month == 4 || 6 || 9 || 11 ) {
-
+    print "month:$month\n";
+    if ( $month =~ /^([469]|11)$/ ) {
+        print "Months with 31 $month\n";
         if ( $user_input =~ /^([0-2][0-9]|3[0-1])/ ) {
-            print "user date is $user_input_1\n";
+            print "user date is $output_date\n";
         }
         else {
             print "wrong date\n";
         }
     }
-    elsif ( $month == 1 || 3 || 5 || 7 || 8 || 10 || 12 ) {
+    elsif ( $month =~ /^([13578]|10|12)$/ ) {
+
+        print "Months with 30 $month\n";
         if ( $user_input =~ /^([0-2][0-9]|30)/ ) {
-            print "user input is $user_input\n";
+            print "user input is $output_date\n";
         }
         else {
             print "wrong date\n";
@@ -42,11 +45,11 @@ if ( $user_input_1 =~ /(\d{2}-\d{2}-\d{4}|\d{2}\/\d{2}\/\d{4}|\d{8})/ ) {
     }
     elsif ( $month == 2 ) {
 
-        my $return_value = leap_year_veri();
-        print "-------$return_value\n";
+        my $return_value = leap_year_veri($user_in);
+
         if ( $return_value == 1 ) {
-            if ( $user_input =~ /^([0-2] [0-9])/ ) {
-                print "user input is $user_input\n";
+            if ( $user_input =~ /^([0-2][0-9])/ ) {
+                print "user input is $output_date\n";
             }
             else {
                 print "wrong date\n";
@@ -55,7 +58,7 @@ if ( $user_input_1 =~ /(\d{2}-\d{2}-\d{4}|\d{2}\/\d{2}\/\d{4}|\d{8})/ ) {
         }
         else {
             if ( $user_input =~ /^([0-2][0-8])/ ) {
-                print "user input is $user_input\n";
+                print "user input is $output_date\n";
             }
             else {
                 print "wrong date\n";
@@ -67,60 +70,34 @@ if ( $user_input_1 =~ /(\d{2}-\d{2}-\d{4}|\d{2}\/\d{2}\/\d{4}|\d{8})/ ) {
         print "wrong month\n";
     }
 
-    sub leap_year_veri {
-
-        $user_in =~ /(\d{4}$)/;
-        my $year = $1;
-        if ( $year % 4 == 0 ) {
-            if ( $year % 100 == 0 ) {
-                if ( $year % 400 == 0 ) {
-                    return 1;
-                }
-                else {
-                    return 0;
-
-                }
-            }
-            else {
-                return 1;
-            }
-        }
-        else {
-
-            return 0;
-
-        }
-
-    }
-
 }
 else {
     print "date format invalid\n";
 }
 
-=pod       
-    sub leap_year_veri{  
-      
-      $user_in=~/(\d{4}$)/ ;
-         my $year=$1;
-       if($year%4==0){
-              if($year%100==0)
-          {       
-             if($year%400==0){
-                  return 1;
-                 }else{
-                       return 0;
-                   
-          }
-}else{
-                   return 1;
+sub leap_year_veri {
+    my $user_in = $_[0];
+    $user_in =~ /(\d{4}$)/;
+    my $year = $1;
+    if ( $year % 4 == 0 ) {
+        if ( $year % 100 == 0 ) {
+            if ( $year % 400 == 0 ) {
+                return 1;
             }
-   }else{
-         
-     return 0;
-  
+            else {
+                return 0;
+
+            }
+        }
+        else {
+            return 1;
+        }
+    }
+    else {
+
+        return 0;
+
+    }
+
 }
 
-
-}
-=cut
